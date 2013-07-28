21355e3f2063e9aa59cdd0fdb7fff6dbd15300dd
Andrea Aime <andrea.aime@gmail.com>
2013-07-28 10:40:21 +0200

    Added a simple way to handle generic rendered images

--- src/main/java/org/geoserver/png/ng/providers/ScanlineProviderFactory.java
+++ src/main/java/org/geoserver/png/ng/providers/ScanlineProviderFactory.java
@@ -10,62 +10,67 @@ import java.awt.image.Raster;
 import java.awt.image.RenderedImage;
 import java.awt.image.SampleModel;
 
+import javax.media.jai.PlanarImage;
+
 public class ScanlineProviderFactory {
 
     public static ScanlineProvider getProvider(RenderedImage image) {
-        if (image instanceof BufferedImage) {
-            BufferedImage bi = (BufferedImage) image;
-            ColorModel cm = image.getColorModel();
-            SampleModel sm = image.getSampleModel();
-            Raster raster = bi.getData();
-            if (cm instanceof ComponentColorModel && sm.getDataType() == DataBuffer.TYPE_BYTE) {
-                if (sm.getNumBands() == 3 || sm.getNumBands() == 4) {
-                    return new RasterByteABGRProvider(raster, bi.getColorModel().hasAlpha());
-                } else if(sm.getNumBands() == 2 && cm.hasAlpha()) {
-                    return new RasterByteGrayAlphaProvider(raster);
-                } else if (sm.getNumBands() == 1) {
-                    if (cm.getPixelSize() == 8) {
-                        return new RasterByteGrayProvider(raster);
-                    } else if (cm.getPixelSize() == 4) {
-                        return new RasterFourBitsGrayProvider(raster);
-                    } else if (cm.getPixelSize() == 2) {
-                        return new RasterTwoBitsGrayProvider(raster);
-                    } else if (cm.getPixelSize() == 1) {
-                        return new RasterOneBitGrayProvider(raster);
-                    }
+        ColorModel cm = image.getColorModel();
+        SampleModel sm = image.getSampleModel();
+        
+        Raster raster;
+        if(image instanceof BufferedImage) {
+            raster = ((BufferedImage) image).getRaster();
+        } else {
+            raster = image.getData();
+        }
 
+        // grab the right scanline extractor based on image features
+        if (cm instanceof ComponentColorModel && sm.getDataType() == DataBuffer.TYPE_BYTE) {
+            if (sm.getNumBands() == 3 || sm.getNumBands() == 4) {
+                return new RasterByteABGRProvider(raster, cm.hasAlpha());
+            } else if (sm.getNumBands() == 2 && cm.hasAlpha()) {
+                return new RasterByteGrayAlphaProvider(raster);
+            } else if (sm.getNumBands() == 1) {
+                if (cm.getPixelSize() == 8) {
+                    return new RasterByteGrayProvider(raster);
+                } else if (cm.getPixelSize() == 4) {
+                    return new RasterFourBitsGrayProvider(raster);
+                } else if (cm.getPixelSize() == 2) {
+                    return new RasterTwoBitsGrayProvider(raster);
+                } else if (cm.getPixelSize() == 1) {
+                    return new RasterOneBitGrayProvider(raster);
                 }
-            } else if (cm instanceof ComponentColorModel
-                    && sm.getDataType() == DataBuffer.TYPE_USHORT) {
-                if (sm.getNumBands() == 3 || sm.getNumBands() == 4) {
-                    return new RasterShortABGRProvider(raster, bi.getColorModel().hasAlpha());
-                } else if (sm.getNumBands() == 2 && cm.hasAlpha()) {
-                    return new RasterShortGrayAlphaProvider(raster);
-                } else if (sm.getNumBands() == 1) {
-                    return new RasterShortGrayProvider(raster);
+
+            }
+        } else if (cm instanceof ComponentColorModel && sm.getDataType() == DataBuffer.TYPE_USHORT) {
+            if (sm.getNumBands() == 3 || sm.getNumBands() == 4) {
+                return new RasterShortABGRProvider(raster, cm.hasAlpha());
+            } else if (sm.getNumBands() == 2 && cm.hasAlpha()) {
+                return new RasterShortGrayAlphaProvider(raster);
+            } else if (sm.getNumBands() == 1) {
+                return new RasterShortGrayProvider(raster);
+            }
+        } else if (cm instanceof DirectColorModel && sm.getDataType() == DataBuffer.TYPE_INT) {
+            if (sm.getNumBands() == 3 || sm.getNumBands() == 4) {
+                return new RasterIntABGRProvider(raster, cm.hasAlpha());
+            } else if (sm.getNumBands() == 1) {
+                return new RasterByteGrayProvider(raster);
+            }
+        } else if (cm instanceof IndexColorModel) {
+            IndexColorModel icm = (IndexColorModel) cm;
+            if (sm.getDataType() == DataBuffer.TYPE_BYTE) {
+                if (icm.getPixelSize() == 8) {
+                    return new RasterByteIndexedProvider(raster, icm);
+                } else if (icm.getPixelSize() == 4) {
+                    return new RasterFourBitsIndexedProvider(raster, icm);
+                } else if (icm.getPixelSize() == 2) {
+                    return new RasterTwoBitsIndexedProvider(raster, icm);
+                } else if (icm.getPixelSize() == 1) {
+                    return new RasterOneBitIndexedProvider(raster, icm);
                 }
-            } else if (cm instanceof DirectColorModel && sm.getDataType() == DataBuffer.TYPE_INT) {
-                if (sm.getNumBands() == 3 || sm.getNumBands() == 4) {
-                    return new RasterIntABGRProvider(raster, bi.getColorModel().hasAlpha());
-                } else if (sm.getNumBands() == 1) {
-                    return new RasterByteGrayProvider(bi.getRaster());
-                }
-            } else if (cm instanceof IndexColorModel) {
-                IndexColorModel icm = (IndexColorModel) cm;
-                if (sm.getDataType() == DataBuffer.TYPE_BYTE) {
-                    if (icm.getPixelSize() == 8) {
-                        return new RasterByteIndexedProvider(raster, icm);
-                    } else if (icm.getPixelSize() == 4) {
-                        return new RasterFourBitsIndexedProvider(raster, icm);
-                    } else if (icm.getPixelSize() == 2) {
-                        return new RasterTwoBitsIndexedProvider(raster, icm);
-                    } else if (icm.getPixelSize() == 1) {
-                        return new RasterOneBitIndexedProvider(raster, icm);
-                    }
-                } else if (sm.getDataType() == DataBuffer.TYPE_USHORT) {
-                    return new RasterShortIndexedProvider(raster,
-                            (IndexColorModel) bi.getColorModel());
-                }
+            } else if (sm.getDataType() == DataBuffer.TYPE_USHORT) {
+                return new RasterShortIndexedProvider(raster, (IndexColorModel) cm);
             }
         }
 
--- src/test/java/org/geoserver/png/PngSuiteImagesTest.java
+++ src/test/java/org/geoserver/png/PngSuiteImagesTest.java
@@ -2,7 +2,9 @@ package org.geoserver.png;
 
 import static org.junit.Assert.assertEquals;
 
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -14,6 +16,10 @@ import java.util.List;
 import java.util.zip.Deflater;
 
 import javax.imageio.ImageIO;
+import javax.media.jai.ImageLayout;
+import javax.media.jai.JAI;
+import javax.media.jai.RenderedOp;
+import javax.media.jai.operator.FormatDescriptor;
 
 import org.geoserver.png.ng.FilterType;
 import org.geoserver.png.ng.PngEncoder;
@@ -56,8 +62,31 @@ public class PngSuiteImagesTest {
     public void testRoundTripFilterNone() throws Exception {
         BufferedImage input = ImageIO.read(sourceFile);
 
+        roundTrip(input, input);
+    }
+    
+    @Test
+    public void testRoundTripTiledImage() throws Exception {
+        BufferedImage input = ImageIO.read(sourceFile);
+        
+        // prepare a tiled image layout
+        ImageLayout il = new ImageLayout(input);
+        il.setTileWidth(8);
+        il.setTileHeight(8);
+        
+        RenderingHints hints = new RenderingHints(JAI.KEY_IMAGE_LAYOUT, il);
+        RenderedOp tiled = FormatDescriptor.create(input, input.getSampleModel().getDataType(), hints);
+        assertEquals(8, tiled.getTileWidth());
+        assertEquals(8, tiled.getTileHeight());
+
+        roundTrip(input, tiled);
+    }
+    
+    
+    
+    private void roundTrip(BufferedImage original, RenderedImage source) throws IOException {
         Deflater deflater = new Deflater();
-        ScanlineProvider scanlines = ScanlineProviderFactory.getProvider(input);
+        ScanlineProvider scanlines = ScanlineProviderFactory.getProvider(source);
         PngEncoder encoder = new PngEncoder(scanlines, deflater, FilterType.None);
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         encoder.encode(bos);
@@ -68,14 +97,14 @@ public class PngSuiteImagesTest {
         ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
         BufferedImage image = ImageIO.read(bis);
 
-        assertEquals(input.getWidth(), image.getWidth());
-        assertEquals(input.getHeight(), image.getHeight());
-        assertEquals(input.getSampleModel(), image.getSampleModel());
-        assertEquals(input.getColorModel(), image.getColorModel());
+        assertEquals(original.getWidth(), image.getWidth());
+        assertEquals(original.getHeight(), image.getHeight());
+        assertEquals(original.getSampleModel(), image.getSampleModel());
+        assertEquals(original.getColorModel(), image.getColorModel());
 
-        for (int x = 0; x < input.getWidth(); x++) {
-            for (int y = 0; y < input.getHeight(); y++) {
-                assertEquals(input.getRGB(x, y), image.getRGB(x, y));
+        for (int x = 0; x < original.getWidth(); x++) {
+            for (int y = 0; y < original.getHeight(); y++) {
+                assertEquals(original.getRGB(x, y), image.getRGB(x, y));
             }
         }
     }
